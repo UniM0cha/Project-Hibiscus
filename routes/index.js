458463d4d87968e5c6ea66403ce3af1131b1b1ec
UniM0cha <solst_ice@naver.com>
2021-11-17 21:42:43 +0900

    1117_웹소켓 테스트

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/gameTest', function(req, res, next) {
+  res.render('game_test');
+});
+
+router.get('/socketTest', function(req, res, next) {
+  res.render('socket_test');
 });
 
 module.exports = router;
